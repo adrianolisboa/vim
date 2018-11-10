f749cc95c6e340d32f8766fa060b89b4ce1760a5
Adriano Lisboa <adrianolic@gmail.com>
2018-11-10 18:03:59 -0200

    Remove folder when configurtion is complete

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 VIM_FOLDER="${HOME}/.vim"
+GIT_DIR="$PWD"
 
 # only execute if there's no vim folder already
 if [ ! -d "${VIM_FOLDER}" ]; then
     mkdir ${VIM_FOLDER}
-    mv $PWD/{.,}* ${VIM_FOLDER}
+    mv ${GIT_DIR}/{.,}* ${VIM_FOLDER}
     ln -s ${VIM_FOLDER}/vimrc ${HOME}/.vimrc
     cd ~/.vim/pack/minpac/opt
     git clone https://github.com/k-takata/minpac.git
-    vim +PackUpdate +q
     cd ${VIM_FOLDER}
+    rm -rf ${GIT_DIR}
+    vim +PackUpdate +q
 fi
 
 echo "Well Done! You vim is now configurated and located at: ${VIM_FOLDER}"
