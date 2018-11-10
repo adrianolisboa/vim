08e4935d20766afc79c5cd95df58fc7e9230a1b9
Adriano Lisboa <adrianolic@gmail.com>
2018-11-10 17:58:24 -0200

    Fix setup direction for moving files

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ VIM_FOLDER="${HOME}/.vim"
 # only execute if there's no vim folder already
 if [ ! -d "${VIM_FOLDER}" ]; then
     mkdir ${VIM_FOLDER}
-    cp -r * ${VIM_FOLDER}
+    mv $PWD/{.,}* ${VIM_FOLDER}
     ln -s ${VIM_FOLDER}/vimrc ${HOME}/.vimrc
     cd ~/.vim/pack/minpac/opt
     git clone https://github.com/k-takata/minpac.git
     vim +PackUpdate +q
+    cd ${VIM_FOLDER}
 fi
 
-echo "Well Done! You can now delete this folder because a copy of it was created in: ${VIM_FOLDER}"
+echo "Well Done! You vim is now configurated and located at: ${VIM_FOLDER}"
